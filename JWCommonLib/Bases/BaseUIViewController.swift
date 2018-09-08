e858c1e74f98a75654ed510b98e35c8fa3d84f1c
amirfahmideh <amir.fahmideh@hotmail.com>
2018-09-08 14:53:15 +0430

    change project to module base

---
+++ JWCommonLib/Bases/BaseUIViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BaseUIViewController.swift
+//  JWCommonLib
+//
+//  Created by Amir Fahmideh on 6/17/1397 AP.
+//  Copyright © 1397 AP Amir Fahmideh. All rights reserved.
+//
+
+import UIKit
+open class BaseUIViewController:UIViewController {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: self.view.window)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: self.view.window)
+    }
+    override open func viewWillAppear (_ animated: Bool){
+        super.viewWillAppear(false)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0{
+                UIView.animate(withDuration: 0.01, animations: { () -> Void in
+                    self.view.frame.origin.y -= keyboardSize.height
+                })
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        UIView.animate(withDuration: 0.2, animations: { () -> Void in
+            self.view.frame.origin.y = 0
+        })
+        
+    }
+}
