a4bedd738d149280b54ae54bae1d61912555cf6c
amirfahmideh <amir.fahmideh@hotmail.com>
2018-10-27 15:56:52 +0330

    fix login project in parmarker

---
+++ JWCommonLib/Images/Base64Image.swift
@@ -0,0 +1,23 @@
+//
+//  Base64.swift
+//  JWCommonLib
+//
+//  Created by Amir Fahmideh on 8/5/1397 AP.
+//  Copyright © 1397 AP Amir Fahmideh. All rights reserved.
+//
+
+import UIKit
+
+public class Base64Image {
+    public static func convertImageToBase64(image: UIImage) -> String {
+        let imageData = image.UIImagePNGRepresentation(_:)
+        return imageData?.base64EncodedString()
+    }
+    //
+    // Convert base64 to String
+    //
+    public static func convertBase64ToImage(imageString: String) -> UIImage {
+        let imageData = Data(base64Encoded: imageString)!
+        return UIImage(data: imageData)!
+    }
+}
