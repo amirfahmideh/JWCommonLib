f9d3a408fe02249da8fb3eb085df673192497b12
amirfahmideh <amir.fahmideh@hotmail.com>
2018-11-08 08:59:22 +0330

    add image button to baseVC

--- JWCommonLib/Controls/TextField/JWRoundedTextField.swift
+++ JWCommonLib/Controls/TextField/JWRoundedTextField.swift
@@ -28,5 +28,17 @@ open class JWRoundedTextField:UITextField {
         }
         
     }
-
+    
+    var padding = UIEdgeInsets(top:10,left:0,bottom:10,right:0)
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
 }
