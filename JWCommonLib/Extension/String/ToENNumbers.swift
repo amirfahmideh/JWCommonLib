9bd40ef1e7b4bf4c3aa2397d7a9eef7a8fffb744
amirfahmideh <amir.fahmideh@hotmail.com>
2018-11-20 14:31:47 +0330

    update some decimal and number formatting

--- JWCommonLib/Extension/String/ToENNumbers.swift
+++ JWCommonLib/Extension/String/ToENNumbers.swift
@@ -1,16 +1,25 @@
 import UIKit
-extension String {
-    func ToENIntNumber() -> Int {
+public extension String {
+    public func ToENIntNumber() -> Int {
         let Formatter = NumberFormatter()
         Formatter.locale = NSLocale(localeIdentifier: "EN") as Locale?
         let final = Formatter.number(from: self)
         return Int(truncating: final!)
     }
-    func ToENInt64Number() -> Int64 {
+    public func ToENInt64Number() -> Int64 {
         let Formatter = NumberFormatter()
         Formatter.locale = NSLocale(localeIdentifier: "EN") as Locale?
         let final = Formatter.number(from: self)
         return Int64(truncating: final!)
     }
 
+    //Decimal
+    public func ToENDecimalNumber() -> Decimal? {
+        let Formatter = NumberFormatter()
+        Formatter.locale = NSLocale(localeIdentifier: "EN") as Locale?
+        Formatter.numberStyle = .decimal;
+        let final = Formatter.number(from: self)
+        let decimalN:String = Formatter.string(from: final!)!
+        return Decimal(string:decimalN)
+    }
 }
