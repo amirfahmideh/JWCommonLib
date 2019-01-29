ae0a062a0189664a7444e551674d7ce20b3498de
amirfahmideh <amir.fahmideh@hotmail.com>
2019-01-29 14:01:01 +0330

    finish login method with changes in post

---
+++ JWCommonLib/UserDefaultStorage/UserDefaultStorage.swift
@@ -0,0 +1,21 @@
+//
+//  UserDefaultStorage.swift
+//  JWCommonLib
+//
+//  Created by Amir Fahmideh on 11/9/1397 AP.
+//  Copyright © 1397 AP Amir Fahmideh. All rights reserved.
+//
+
+import UIKit
+public class UserDefaultStorage {
+    public static func Save(key:String, value:Any){
+        let defaults = UserDefaults.standard
+        defaults.set(value, forKey: key)
+        defaults.synchronize()
+    }
+    public static func Get<T>(type:T.Type, key:String, value:Any) -> T{
+        let defaults = UserDefaults.standard
+        return defaults.object(forKey: key) as! T
+    }
+
+}
